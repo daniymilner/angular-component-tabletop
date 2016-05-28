5014e2b556b1c189ed5a6835d12d1ebbc5a2b85e
Aleksey Olenyuk <alo@qapint.com>
2016-05-28 10:44:15 +0300

    refactor table layout

--- public/js/controllers/table.ctrl.js
+++ public/js/controllers/table.ctrl.js
@@ -13,12 +13,28 @@
 					.getGallery()
 					.success(function(data){
 						that.data = data;
+						if(that.data.companies.length){
+							that.active = that.data.companies[0].company_name;
+						}
 					})
 					.error(console.error);
 
 				this.toggle = function(item, company){
 					Table.toggle(item, company);
 				};
+
+				this.setActive = function(name){
+					this.active = name;
+				};
+
+				this.getIndex = function(name){
+					for(var i = 0; i < that.data.companies.length; i++){
+						if(that.data.companies[i].company_name === name){
+							return i;
+						}
+					}
+					return 0;
+				}
 			}
 		])
 })();
